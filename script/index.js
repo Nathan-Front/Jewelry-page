023f32e6596bc6241d85f6f36df742e1c2135f7b
Nathan-Front <jonathantindungan3@gmail.com>
2026-04-19 15:32:04 +0900

    added secondSection for home html

--- script/index.js
+++ script/index.js
@@ -7,13 +7,17 @@ async function fetchHTML() {
     const resHome = await fetch("./page/home/home.html");
     const homeHTML = await resHome.text();
     const parser = new DOMParser();
-    const home = parser.parseFromString(homeHTML, "text/html")
+    const home = parser.parseFromString(homeHTML, "text/html");
     const homeMain = home.getElementById("home-main");
     body.appendChild(homeMain);
 
-    const resFirst = await fetch("./page/home/firstSection.html")
+    const resFirst = await fetch("./page/home/firstSection.html");
     const firstHTML = await resFirst.text();
     homeMain.insertAdjacentHTML("afterbegin", firstHTML);
+
+    const resSecond = await fetch("./page/home/secondSection.html");
+    const secondHTML = await resSecond.text();
+    homeMain.insertAdjacentHTML("afterend", secondHTML);
 
     const resFoot = await fetch("./components/footer/footer.html");
     const footHTML = await resFoot.text();
