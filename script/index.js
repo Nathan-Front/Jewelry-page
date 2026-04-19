098d30f4728e3cecbaa07a7825ad4cb516c8fc9f
Nathan-Front <jonathantindungan3@gmail.com>
2026-04-19 12:15:17 +0900

    first commit

---
+++ script/index.js
@@ -0,0 +1,23 @@
+async function fetchHTML() {
+    const body = document.body;
+    const resNav = await fetch("./components/navigation/navigation.html");
+    const navHTML = await resNav.text();
+    body.insertAdjacentHTML("afterbegin", navHTML);
+
+    const resHome = await fetch("./page/home/home.html");
+    const homeHTML = await resHome.text();
+    const parser = new DOMParser();
+    const home = parser.parseFromString(homeHTML, "text/html")
+    const homeMain = home.getElementById("home-main");
+    body.appendChild(homeMain);
+
+    const resFirst = await fetch("./page/home/firstSection.html")
+    const firstHTML = await resFirst.text();
+    homeMain.insertAdjacentHTML("afterbegin", firstHTML);
+
+    const resFoot = await fetch("./components/footer/footer.html");
+    const footHTML = await resFoot.text();
+    body.insertAdjacentHTML("beforeend", footHTML);
+}
+document.addEventListener("DOMContentLoaded", fetchHTML);
+
